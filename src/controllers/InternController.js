704ba583547dc17505ea5c8a5a8fedc346091482
Functionup-sg <sujatagarande@gmail.com>
2022-09-14 16:18:36 +0530

    commit

--- src/controllers/InternController.js
+++ src/controllers/InternController.js
@@ -1,36 +1,40 @@
 const collegeModel = require('../Models/CollegeModel')
 const internModel = require('../Models/InternModel')
-const { isValid, isValidName } = require('../Validator/validation')
+const { isValidreqbody, isValid, isValidName, isValidMobile, isValidEmail } = require('../Validator/validation')
 
 const createInterns = async function (req, res) {
-    const { name, mobile, email, collegeName } = req.body
-    if (!collegeName) {
-        return res.status(400).send({ status: false, msg: "collegeName can't be empty" })
-    }
     try {
+        
+        const { collegeName, name, mobile, email } = req.body
+
         const college = await collegeModel.findOne({ name: collegeName });
         if (!college) {
             return res.status(400).send({ status: false, msg: "College not found" })
         }
 
-        if (!isValid(name)) {
-            return res.status(400).send({ status: false, msg: "name can't be empty" })
-        }
-        if (!isValidName(name)) {
-            return res.status(400).send({ status: false, msg: "please provide a valid name" })
-        }
-        if (!mobile) {
-            return res.status(400).send({ status: false, msg: "mobile can't be empty" })
-        }
-        if (!email) {
-            return res.status(400).send({ status: false, msg: "email can't be empty" })
-        }
+        if (!isValidreqbody) { return res.status(400).send({ status: false, msg: "Body should not be empty " })}
 
-        const intern = await internModel.create({ name, mobile, email, collegeID: college._id })
-        return res.status(200).send({ data: intern })
+        if (!isValid(collegeName)) {return res.status(400).send({ status: false, msg: "collegeName can't be empty" }) }
+
+        if (!isValid(name)) {return res.status(400).send({ status: false, msg: "name can't be empty" })}
+
+        if (!isValidName(name)) {return res.status(400).send({ status: false, msg: "please provide a valid name" })}
+
+        if (!isValid(mobile)) {return res.status(400).send({ status: false, msg: "mobile can't be empty" })}
+
+        if (!isValidMobile(mobile)) {return res.status(400).send({ status: false, msg: "please provide a valid mobile" })}
+
+        if (!isValid(email)) {return res.status(400).send({ status: false, msg: "email can't be empty" })}
+
+        if (!isValidEmail(email)) {return res.status(400).send({ status: false, msg: "please provide a valid email" })}
+
+        const createdData = await internModel.create({ name, mobile, email, collegeID: college._id })
+        return res.status(200).send({ data: createdData })
+
     } catch (error) {
-        return res.status(500).send({ status: false, msg: "Something went wrong" })
+        console.log(error)
+        return res.status(500).send({ msg: error.message })
     }
-}
+} 
 
 module.exports.createInterns = createInterns
